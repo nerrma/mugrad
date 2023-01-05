2b4a6c3ea332654d834cd5cbd1b802bfec13f4a9
Sina <sina.alsharif@gmail.com>
2023-01-05 22:08:54 +1100

    fix activation issues

--- micrograd_py/nn.py
+++ micrograd_py/nn.py
@@ -1,31 +1,39 @@
 #!/usr/bin/env python3
 
 import random
-import autograd as ag
+import autodiff as ad
 
 
 class Neuron:
-    def __init__(self, n_in, w=None, b=None, activation=None, layer=-1, neuron=-1):
+    def __init__(self, n_in, w=None, b=None, active=True, activation=None):
         if w is None:
-            self.w = [
-                ag.Value(random.uniform(0, 1.0), label=f"{layer}_{neuron}_w{i}")
-                for i in range(n_in)
-            ]
+            self.w = [ad.Value(random.uniform(-1.0, 1.0)) for i in range(n_in)]
+        else:
+            self.w = w
 
         if b is None:
-            self.b = ag.Value(0.0, label=f"b")
+            self.b = ad.Value(0.0, label=f"b")
+        else:
+            self.b = b
 
         self.activation = activation
+        self.active = active
         assert len(self.w) == n_in
 
     def __call__(self, x):
         assert len(x) == len(self.w)
         act = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
 
-        # return act
-        return act.relu() if self.activation else act
+        if not self.active:
+            return act
+        elif self.activation == "relu":
+            return act.relu()
+        elif self.activation == "tanh":
+            return act.tanh()
+        else:
+            return act
 
-    def params(self):
+    def parameters(self):
         return self.w + [self.b]
 
 
@@ -33,37 +41,34 @@ class Layer:
     def __init__(self, dims, weights=None, biases=None, **kwargs):
         if weights and biases:
             self.neurons = [
-                Neuron(dims[0], w=ag.Value(w), b=ag.Value(b), **kwargs)
+                Neuron(dims[0], w=ad.Value(w), b=ad.Value(b), **kwargs)
                 for w, b in zip(weights, biases)
             ]
         else:
-            self.neurons = [Neuron(dims[0], neuron=i, **kwargs) for i in range(dims[1])]
-        self.dims = dims
+            self.neurons = [Neuron(dims[0], **kwargs) for i in range(dims[1])]
 
     def __call__(self, x):
         out = [N(x) for N in self.neurons]
         return out[0] if len(out) == 1 else out
 
-    def params(self):
-        return [p for n in self.neurons for p in n.params()]
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
 
 
 class MLP:
-    def __init__(self, dims=[(2, 1), (1, 1)], **kwargs):
-        self.layers = []
-        for i, d in enumerate(dims):
-            self.layers.append(Layer(d, activation=True, layer=i, **kwargs))
+    def __init__(self, dims, **kwargs):
+        self.layers = [
+            Layer(d, active=(i != len(dims) - 1), **kwargs) for i, d in enumerate(dims)
+        ]
 
-        self.layers[-1].activation = False
-
-    def forward(self, x):
+    def __call__(self, x):
         for l in self.layers:
             x = l(x)
         return x
 
-    def params(self):
-        return [p for l in self.layers for p in l.params()]
+    def parameters(self):
+        return [p for l in self.layers for p in l.parameters()]
 
     def zero_grad(self):
-        for p in self.params():
+        for p in self.parameters():
             p.grad = 0
