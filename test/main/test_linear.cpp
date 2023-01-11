9af1e8290c9da65ca0c3f0c0f4ebfbdb47e6c8d6
Sina <sina.alsharif@gmail.com>
2023-01-11 22:42:20 +1100

    basic test for addition

--- test/main/test_linear.cpp
+++ test/main/test_linear.cpp
@@ -1,7 +1,23 @@
 #include "value/value.hpp"
 #include <catch2/catch.hpp>
 
-TEST_CASE("basic test") {
-  auto v = mugrad::value(2.0);
-  CHECK(v.get_s() == 2.0);
+TEST_CASE("basic constructor test") {
+  auto v = mugrad::value(2.0, "v1");
+  CHECK(v.get_data() == 2.0);
+}
+
+TEST_CASE("basic addition test") {
+  auto v1 = mugrad::value(2.0, "v1");
+  auto v2 = mugrad::value(3.0, "v2");
+  auto res = v1 + v2;
+  CHECK(res.get_data() == 5.0);
+}
+
+TEST_CASE("basic addtion backward test") {
+  auto v1 = mugrad::value(2.0, "v1");
+  auto v2 = mugrad::value(3.0, "v2");
+  auto res = v1 + v2;
+  res.backward();
+
+  CHECK(res.get_grad() == 1.0);
 }
