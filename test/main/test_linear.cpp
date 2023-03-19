ac5077a459e5d356e4f09f67389985d419f9a20e
Sina <sina.alsharif@gmail.com>
2023-03-19 23:14:47 +1100

    more tests

--- test/main/test_linear.cpp
+++ test/main/test_linear.cpp
@@ -1,3 +1,4 @@
+#include "expr/expr.hpp"
 #include "value/value.hpp"
 #include <catch2/catch.hpp>
 
@@ -36,16 +37,39 @@ TEST_CASE("multiple layer backward") {
   auto v2 = std::make_shared<mugrad::Node>(mugrad::Value(2.0, "v2"));
   auto v3 = std::make_shared<mugrad::Node>(mugrad::Value(3.0, "v3"));
   auto v4 = std::make_shared<mugrad::Node>(mugrad::Value(4.0, "v4"));
-  // auto res = v1 * v2 + v3 * v2 + v4 * v2;
   auto res = v1 * v2 * v3 * v4;
   CHECK(res->get_data() == 24.0);
 
-  auto topo = res->gen_topo(res);
-
-  res->set_grad(1);
-
-  for (auto const &c : topo)
-    c->backward();
-
+  mugrad::backward(res);
   CHECK(v2->get_grad() == 12.0);
+}
+
+TEST_CASE("multiple operation backward") {
+  auto v1 = std::make_shared<mugrad::Node>(mugrad::Value(1.0, "v1"));
+  auto v2 = std::make_shared<mugrad::Node>(mugrad::Value(2.0, "v2"));
+  auto v3 = std::make_shared<mugrad::Node>(mugrad::Value(3.0, "v3"));
+  auto v4 = std::make_shared<mugrad::Node>(mugrad::Value(4.0, "v4"));
+  auto res = v1 * v2 + v3 * v2 + v4 * v2;
+  CHECK(res->get_data() == 16.0);
+
+  mugrad::backward(res);
+  CHECK(v2->get_grad() == 8.0);
+}
+
+TEST_CASE("layered backward") {
+  auto v1 = std::make_shared<mugrad::Node>(mugrad::Value(3.0, "v1"));
+  auto v2 = std::make_shared<mugrad::Node>(mugrad::Value(4.0, "v2"));
+
+  auto res = v1 * v2 + v1;
+  CHECK(res->get_data() == 15.0);
+
+  auto res2 = res * v2 + res;
+  CHECK(res2->get_data() == 75.0);
+
+  /*
+    res2 = v2 * (v1 * v2 + v1) + v1 * v2 + v1
+    dres2/dv2 = 2 * v1 * v2 + v1 + v1
+   */
+  mugrad::backward(res2);
+  CHECK(v2->get_grad() == 30.0);
 }
